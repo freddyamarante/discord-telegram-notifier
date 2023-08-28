2824be6b101a9f09fe840eb8ccd60f74a72a00f7
Freddy Amarante <freddyamarante14@gmail.com>
2023-08-28 15:12:56 -0400

    feat: add discord.js

--- app.js
+++ app.js
@@ -1,60 +1,10 @@
-const WebSocket = require('ws')
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+const { token } = require('./config.json')
 
-const ws = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json')
-let interval = 0
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-token = 'MjMxMTQ4Njc3OTM3MjMzOTMw.GRVwCn.1WnFhCpYcgd2gb-BP6hi9hoVVzdshavV4fll08'
-payload = {
-  op: 2,
-  d: {
-    token,
-    intents: 513,
-    properties: {
-      $os: 'linux',
-      $browser: 'chrome',
-      $device: 'chrome',
-    },
-  },
-}
-
-ws.on('open', function open() {
-  ws.send(JSON.stringify(payload))
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
 })
 
-ws.on('message', function incoming(data) {
-  let payload = JSON.parse(data)
-  const { t, d, op } = payload
-
-  switch (op) {
-    case 10:
-      const { heartbeat_interval } = d
-      interval = heartbeat(heartbeat_interval)
-      break
-  }
-
-  switch (t) {
-    case 'READY':
-      console.log('ready!')
-      break
-    case 'VOICE_STATE_UPDATE':
-      const { user_id, member, channel_id } = d
-      console.log('it changed')
-
-      if (channel_id) {
-        console.log(`${member.user.username} has entered the channel.`)
-      }
-      break
-    case 'TYPING_START':
-      console.log('somebody started typing dawg')
-  }
-})
-
-ws.on('error', function error(err) {
-  console.error('WebSocket error:', err)
-})
-
-const heartbeat = (ms) => {
-  return setInterval(() => {
-    ws.send(JSON.stringify({ op: 2, d: null }))
-  }, ms)
-}
+client.login(token)
