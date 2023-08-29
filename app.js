13ff60daa39d63b8dc5e6e72e837ba2ecf3af4be
freddy <freddyamarante14@gmail.com>
2023-08-29 14:19:04 -0400

    feat: create events folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const { Client, Events, Collection, GatewayIntentBits } = require('discord.js')
-const { clientId, guildId, token } = require('./config.json')
+const { Client, Collection, GatewayIntentBits } = require('discord.js')
+const { token } = require('./config.json')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
@@ -17,7 +17,6 @@ for (const folder of commandFolders) {
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command)
     } else {
@@ -28,37 +27,4 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`)
-})
-
-// Login
 client.login(token)
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return
-
-  const command = interaction.client.commands.get(interaction.commandName)
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`)
-    return
-  }
-
-  try {
-    await command.execute(interaction)
-  } catch (error) {
-    console.error(error)
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      })
-    } else {
-      await interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      })
-    }
-  }
-})
